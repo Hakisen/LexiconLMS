45bfd9b0e0d71b56759411dd3dd98079a606bc7a
Stefan Willgert <stefan.willgert@gmail.com>
2019-03-12 10:03:32 +0100

    Ändrat displayname student

--- LexiconLMS/Models/ApplicationUser.cs
+++ LexiconLMS/Models/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,10 @@ namespace LexiconLMS.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Display(Name = "Namn")]
         public string Name { get; set; }
         //public string TelNr { get; set; }
-
+        [Display(Name = "Kurs")]
         public Course Course { get; set; }
         public int? CourseId { get; set; }
     }
