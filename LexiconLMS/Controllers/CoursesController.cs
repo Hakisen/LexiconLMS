a64c46e5486fe88444d66b7002fb04c5d5113433
Håkan Kilström <hakan.kilstrom@outlook.com>
2019-03-15 11:53:56 +0100

    Added authorization to CourseController and changed views

--- LexiconLMS/Controllers/CoursesController.cs
+++ LexiconLMS/Controllers/CoursesController.cs
@@ -12,7 +12,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace LexiconLMS.Controllers
 {
-    //[Authorize(Roles = "Teacher, Student")]
+    [Authorize]
     public class CoursesController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -20,24 +20,26 @@ namespace LexiconLMS.Controllers
 
         public CoursesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
-            
             _context = context;
-          _userManager = userManager;
+            _userManager = userManager;
         }
 
         // GET: Courses
+        [Authorize(Roles = "Teacher")]
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Course.Include(a=>a.ApplicationUser).ToListAsync());
+            return View(await _context.Course.Include(a => a.ApplicationUser).ToListAsync());
         }
         // GET: Students per course
+        [Authorize(Roles = "Teacher, Student")]
         public IActionResult StudentsPerCourse(int? id)
         {
-            var studentspercourse= _context.Course.Include(a => a.ApplicationUser).
+            var studentspercourse = _context.Course.Include(a => a.ApplicationUser).
                  FirstOrDefault(u => u.Id == id);
             return View(studentspercourse);
         }
         // GET: Modules per course
+        [Authorize(Roles = "Teacher, Student")]
         public async Task<IActionResult> StudentModules(int? id)
         {
             var student = new StudentModulesViewModel();
@@ -46,7 +48,7 @@ namespace LexiconLMS.Controllers
             var courseId = (int)user.CourseId;
 
 
-            student.StudentCourse = await _context.Course.Include(a => a.ApplicationUser).Include( u=>u.Modules  ).FirstOrDefaultAsync(u => u.Id == courseId);
+            student.StudentCourse = await _context.Course.Include(a => a.ApplicationUser).Include(u => u.Modules).FirstOrDefaultAsync(u => u.Id == courseId);
             student.Student = user;
 
 
@@ -54,12 +56,13 @@ namespace LexiconLMS.Controllers
         }
 
         //Get:Course students and student and student
+        [Authorize(Roles = "Teacher, Student")]
         public async Task<IActionResult> Student()
         {
             var student = new StudentViewModel();
             var studentName = User.Identity.Name;
             var user = await _userManager.FindByNameAsync(studentName);
-            var courseId = (int) user.CourseId;
+            var courseId = (int)user.CourseId;
 
 
             student.StudentCourse = await _context.Course.Include(a => a.ApplicationUser).FirstOrDefaultAsync(u => u.Id == courseId);
@@ -69,8 +72,9 @@ namespace LexiconLMS.Controllers
             return View(student);
         }
 
-            // GET: Courses/Details/5
-            public async Task<IActionResult> Details(int? id)
+        // GET: Courses/Details/5
+        [Authorize(Roles = "Teacher, Student")]
+        public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
             {
@@ -88,6 +92,7 @@ namespace LexiconLMS.Controllers
         }
 
         // GET: Courses/Create
+        [Authorize(Roles = "Teacher")]
         public IActionResult Create()
         {
             return View();
@@ -100,16 +105,27 @@ namespace LexiconLMS.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,EndDate,StartDate,Description,Name")] Course course)
         {
-            if (ModelState.IsValid)
+            if (course.StartDate.Date <= course.EndDate.Date)
             {
-                _context.Add(course);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                if (ModelState.IsValid)
+                {
+                    _context.Add(course);
+                    await _context.SaveChangesAsync();
+                    TempData["SuccessText"] = $"Kurs: {course.Name} skapades Ok!";
+                    return RedirectToAction(nameof(Index));
+                }
+                TempData["FailText"] = "Något gick fel vid skapandet av modulen. Försök igen";
+                return View(course);
             }
-            return View(course);
+            else
+            {
+                TempData["FailText"] = "Startdatum kan inte ligga senare än slutdatum!";
+                return View(course);
+            }
         }
 
         // GET: Courses/Edit/5
+        [Authorize(Roles = "Teacher")]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -128,6 +144,7 @@ namespace LexiconLMS.Controllers
         // POST: Courses/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "Teacher")]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,EndDate,StartDate,Description,Name")] Course course)
@@ -161,6 +178,7 @@ namespace LexiconLMS.Controllers
         }
 
         // GET: Courses/Delete/5
+        [Authorize(Roles = "Teacher")]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -179,6 +197,7 @@ namespace LexiconLMS.Controllers
         }
 
         // POST: Courses/Delete/5
+        [Authorize(Roles = "Teacher")]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
@@ -194,7 +213,7 @@ namespace LexiconLMS.Controllers
             return _context.Course.Any(e => e.Id == id);
         }
 
-        
-        }
+
     }
+}
 
