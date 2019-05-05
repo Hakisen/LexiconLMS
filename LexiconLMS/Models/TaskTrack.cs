3e1e907fd08074b259cba042ee458bf90f50784e
stefanwil <stefan.willgert@gmail.com>
2019-05-05 20:41:05 +0200

    UYppgiftstöd tillagt

--- LexiconLMS/Models/TaskTrack.cs
+++ LexiconLMS/Models/TaskTrack.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,21 @@ namespace LexiconLMS.Models
 {
     public class TaskTrack
     {
+
+
+        // public List<ApplicationUser> ApplicationUsers { get; set; }
+        [Display(Name = "Elevkurs")]
+        public List<Course> Courses { get; set; }
+        public List<Module> Modules { get; set; }
+        public List<LmsTask> StudentTasks { get; set; }
+
+        [Display(Name = "Elev")]
+        public ApplicationUser Student { get; set; }
+        public List<Document> TaskDocuments { get; set; }
+    
+        public Document ActivityDocument { get; set; }
+    
+
     }
 }
+
